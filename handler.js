ef3725353ed59c302ccf8dd2f9383d90c0ff1574
Errietta Kostala <errietta@errietta.me>
2018-11-12 23:42:21 +0000

    initial commit

--- handler.js
+++ handler.js
@@ -3,20 +3,21 @@ const queueWrapper = require('./lib/sqs-wrapper');
 
 module.exports.addToQueue = async (event, context) => {
   const queue = process.env.SQS_QUEUE_URL;
-  const region = process.env.AWS_REGION;
+  const region = process.env.REGION;
+  const body = JSON.parse(event.body);
 
   await queueWrapper.addToQueue({
     queue,
     region,
     message: {
-      "email": event.body.email,
-      "name": event.body.name,
-      "type": event.body.type || 'reset-password'
+      "email": body.email,
+      "name": body.name,
+      "type": body.type || 'reset_password'
     },
   });
 
   return {
     statusCode: 200,
-    body: 'OK',
+    body: JSON.stringify({ ok: true })
   };
 };
